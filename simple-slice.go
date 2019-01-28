dcc4652431f465cdb34106f9932257777ca69d1a
ZFM <874051547@qq.com>
2019-01-28 23:15:39 +0800

    Update simple-slice.go

--- simple-slice.go
+++ simple-slice.go
@@ -5,7 +5,24 @@ import "fmt"
 func main() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 
-	var s []int = primes[1:4]
-	fmt.Println(len(s))
-	fmt.Println(s)
+	var j []int = primes[1:4]
+	fmt.Println(len(j))
+	fmt.Println(j)
+	
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+	// 截取切片使其长度为 0
+	s = s[:0]
+	printSlice(s)
+	// 拓展其长度
+	s = s[:4]
+	printSlice(s)
+	// 舍弃前两个值 s[0]、s[1] 舍去 从从头数 不关注尾部
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)//cap求容量
+	
 }
