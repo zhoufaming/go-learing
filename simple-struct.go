3b16e5c7b82ee0dc4040dfbaaae757cbb8db8956
ZFM <874051547@qq.com>
2019-01-28 20:11:11 +0800

    Update simple-struct.go

--- simple-struct.go
+++ simple-struct.go
@@ -1,10 +1,6 @@
 package main
 
-
-
 import "fmt"
-
-
 
 type Vertex struct { //结构体的定义
 
@@ -24,18 +20,21 @@ func print(vertex Vertex){
 
 func main() {
 
-	vertex := Vertex{X: 12, Y: 13, z: "12"} 结构体的声明
-
+	vertex := Vertex{X: 12, Y: 13, z: "12"} //结构体的声明
 	vertex.z="jake" //结构体中变量的访问与赋值
-
 	fmt.Println(vertex)
 
- var vertex1 Vertex //声明vertex1为Vertex类型
-
+	var vertex1 Vertex //声明vertex1为Vertex类型
 	vertex1.X = 123
-
 	vertex1.Y =  456
-
 	print(vertex1)
+
+	vertex2 := &vertex1
+	fmt.Println(*vertex2)
+
+	var vertex3 *Vertex //指针变量可以存储结构体变量的地址
+	vertex3 = &vertex
+	fmt.Println(*vertex3)
+
 
 }
