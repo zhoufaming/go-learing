a97c3adbc30bb6a5ea536eb811b29bf5575a82ab
ZFM <874051547@qq.com>
2019-02-13 09:38:12 +0800

    Update simple-method.go

--- simple-method.go
+++ simple-method.go
@@ -72,15 +72,21 @@ func main() {
 	fmt.Println(Color(WHITE),YELLOW)
 
 	//method继承
-	mark := Student{Human{"Mark", 25, "222-222-YYYY"}, "MIT"}
-	sam := Employee{Human{"Sam", 45, "111-888-XXXX"}, "Golang Inc"}
+	mark := Student{Human{"jake", 25, "man","18237369943"}, "utef"}
+	sam := Employee{Human{"tom", 45, "woman","18327468827"}, "tydic"}
 
-	mark.SayHi()
-	sam.SayHi()
+	mark.Ask()
+	sam.Ask()
 }
 
 
 type Human struct {
+	name string
+	age int
+	sex string
+	phone string
+}
+type Human2 struct {
 	name string
 	age int
 	phone string
@@ -90,11 +96,15 @@ type Student struct {
 	Human //匿名字段
 	school string
 }
+type Student2 struct {
+	Human2
+	school string
+}
 
 type Employee struct {
 	Human //匿名字段
 	company string
 }
-func (h *Human) SayHi() {
+func (h *Human) Ask() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
