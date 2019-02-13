d699288d59de6fe3adb3cc2983f09a6d272e241c
ZFM <874051547@qq.com>
2019-02-13 09:40:39 +0800

    Update simple-method.go

--- simple-method.go
+++ simple-method.go
@@ -108,3 +108,9 @@ type Employee struct {
 func (h *Human) Ask() {
 	fmt.Printf("Hi, I am %s you can call me on %s\n", h.name, h.phone)
 }
+
+//Employee的method重写Human的method
+func (e *Employee) Ask() {
+	fmt.Printf("Hi, I am %s, I work at %s. Call me on %s\n", e.name,
+		e.company, e.phone) //Yes you can split into 2 lines here.
+}
