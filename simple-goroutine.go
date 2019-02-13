1ef21841576822240b02ebec35697fbe03167291
ZFM <874051547@qq.com>
2019-02-13 14:53:31 +0800

    Update simple-goroutine.go

--- simple-goroutine.go
+++ simple-goroutine.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 
 
@@ -12,6 +15,29 @@ func sum(a []int, c chan int) {
 	c <- total  // send total to c
 }
 
+func fibonacci(n int, c chan int) {
+	x, y := 1, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x + y
+	}
+	close(c)
+}
+func fibonacci2(c, quit chan int) {
+	x, y := 1, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x + y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		case <- time.After(5 * time.Second):
+			println("timeout")
+			break
+		}
+	}
+}
 
 func set(a string,c chan string){
     c <- a
@@ -31,11 +57,22 @@ func main(){
 	c := make(chan int)
 	go sum(a[:len(a)/2], c)
 	go sum(a[len(a)/2:], c)
-	go sum(a[len(a)/2:], c)
-	go sum(a[len(a)/2:], c)
 	x, y:= <-c, <-c  // receive from c
-
 	fmt.Println(x, y,  x + y)
+    ci := make(chan int,10)
+	go fibonacci(cap(ci), ci)
+	for i := range ci {
+		fmt.Println(i)
+	}
+	cj := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-cj)
+		}
+		quit <- 0
+	}()
+	fibonacci2(cj, quit)
 
 
 }
